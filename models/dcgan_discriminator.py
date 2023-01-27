acf47e6b10b972492795e9ab80d14bb892818941
Boban Stanisljevic <boban00.bs@gmail.com>
2023-01-27 22:38:38 +0100

    Initial DCGAN structure

---
+++ models/dcgan_discriminator.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from keras.layers import Dense, Reshape, Flatten, Conv2D, Conv2DTranspose, ReLU, LeakyReLU, Dropout
+
+
+class Discriminator(tf.keras.Model):
+    def __init__(self, in_shape):
+        super().__init__()
+        self.in_shape = in_shape
+        self.leaky_relu1 = LeakyReLU(alpha=0.2)
+        self.leaky_relu2 = LeakyReLU(alpha=0.2)
+        self.leaky_relu3 = LeakyReLU(alpha=0.2)
+        self.cnv1_layer = Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2), padding='same', input_shape=in_shape)
+        self.cnv2_layer = Conv2D(filters=128, kernel_size=(4, 4), strides=(2, 2), padding='same', input_shape=in_shape)
+        self.cnv3_layer = Conv2D(filters=128, kernel_size=(4, 4), strides=(2, 2), padding='same', input_shape=in_shape)
+        self.output_layer = Dense(1, activation='sigmoid')
+
+    def call(self, input_tensor, training=False):
+        x = self.cnv1_layer(input_tensor, training=training)
+        x = self.leaky_relu1(x)
+        x = self.cnv2_layer(x, training=training)
+        x = self.leaky_relu2(x)
+        x = self.cnv3_layer(x, training=training)
+        x = self.leaky_relu3(x)
+        x = Flatten()(x)
+        x = Dropout(0.3)(x)
+        return self.output_layer(x)
---
+++ models/utils.py
@@ -0,0 +1,60 @@
+# utils.py
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def real_samples(n, dataset):
+    # Samples of real data
+    X = dataset[np.random.choice(dataset.shape[0], n, replace=True), :]
+
+    # Class labels
+    y = np.ones((n, 1))
+    return X, y
+
+
+def latent_vector(latent_dim, n):
+    # Generate points in the latent space
+    latent_input = np.random.randn(latent_dim * n)
+
+    # Reshape into a batch of inputs for the network
+    latent_input = latent_input.reshape(n, latent_dim)
+    return latent_input
+
+
+def fake_samples(generator, latent_dim, n):
+    # Generate points in latent space
+    latent_output = latent_vector(latent_dim, n)
+    # Predict outputs (i.e., generate fake samples)
+    X = generator(latent_output)
+    # Create class labels
+    y = np.zeros((n, 1))
+    return X, y
+
+
+def performance_summary(generator, discriminator, dataset, latent_dim, n=50):
+    # Get samples of the real data
+    x_real, y_real = real_samples(n, dataset)
+    # Evaluate the descriminator on real data
+    _, real_accuracy = discriminator.evaluate(x_real, y_real, verbose=0)
+
+    # Get fake (generated) samples
+    x_fake, y_fake = fake_samples(generator, latent_dim, n)
+    # Evaluate the descriminator on fake (generated) data
+    _, fake_accuracy = discriminator.evaluate(x_fake, y_fake, verbose=0)
+
+    # summarize discriminator performance
+    print("*** Evaluation ***")
+    print("Discriminator Accuracy on REAL images: ", real_accuracy)
+    print("Discriminator Accuracy on FAKE (generated) images: ", fake_accuracy)
+
+    # Display 6 fake images
+    x_fake_inv_trans = x_fake.numpy().reshape(-1, 1)
+    x_fake_inv_trans = x_fake_inv_trans.reshape(n, 64, 64, 3)
+
+    fig, axs = plt.subplots(2, 3, sharey=False, tight_layout=True, figsize=(12, 6), facecolor='white')
+    k = 0
+    for i in range(0, 2):
+        for j in range(0, 3):
+            axs[i, j].matshow(x_fake_inv_trans[k])
+            k = k + 1
+    plt.show()
